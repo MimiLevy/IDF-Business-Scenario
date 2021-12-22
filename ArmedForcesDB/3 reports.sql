580afb662aea7c87c930665c29faaa0a96824e78
Shmiel Minz <s0583223395@gmail.com>
2021-12-22 22:35:34 +0000

    Small update

--- ArmedForcesDB/3 reports.sql
+++ ArmedForcesDB/3 reports.sql
@@ -10,13 +10,13 @@ from ArmedForces af
 order by af.AgeAtEnlistment desc
 
 select 
-    YearRange = case when year(af.DateOfEnlistment) between 2017 and 2019 then 'Year Range (2017-2019)' else '' end,
+    YearRange = 'Year Range (2017-2019)',
     SoldierDetails = concat(af.SoldierFirstName,' ',af.SoldierLastName,' - ',af.SSN,' (',af.DateOfEnlistment,')'),
     af.ServiceUnit
 from ArmedForces af
 where year(af.DateOfEnlistment) between 2017 and 2019
 union select 
-    YearRange = case when year(af.DateOfEnlistment) between 2020 and 2022 then 'Year Range (2020-2022)' else '' end,
+    YearRange = 'Year Range (2020-2022)',
     SoldierDetails = concat(af.SoldierFirstName,' ',af.SoldierLastName,' - ',af.SSN,' (',af.DateOfEnlistment,')'),
     af.ServiceUnit
 from ArmedForces af
