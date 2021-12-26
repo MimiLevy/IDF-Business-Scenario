e9b004c46955c9295d4dfab9d38571dba50ab2c8
Shmiel Minz <s0583223395@gmail.com>
2021-12-26 15:56:45 +0000

    Changed spec

--- ArmedForcesDB/3 reports.sql
+++ ArmedForcesDB/3 reports.sql
@@ -10,15 +10,13 @@ from ArmedForces af
 order by af.AgeAtEnlistment desc
 
 select 
-    YearRange = 'Year Range (2017-2019)',
-    SoldierDetails = concat(af.SoldierFirstName,' ',af.SoldierLastName,' - ',af.SSN,' (',af.DateOfEnlistment,')'),
-    af.ServiceUnit
+    YearRange = '2017-2019',
+    SoldierDetails = concat(af.SoldierFirstName,' ',af.SoldierLastName,' - ',af.SSN,' (',af.DateOfEnlistment,'), ',af.ServiceUnit)
 from ArmedForces af
 where year(af.DateOfEnlistment) between 2017 and 2019
 union select 
-    YearRange = 'Year Range (2020-2022)',
-    SoldierDetails = concat(af.SoldierFirstName,' ',af.SoldierLastName,' - ',af.SSN,' (',af.DateOfEnlistment,')'),
-    af.ServiceUnit
+    YearRange = '2020-2022',
+    SoldierDetails = concat(af.SoldierFirstName,' ',af.SoldierLastName,' - ',af.SSN,' (',af.DateOfEnlistment,'), ',af.ServiceUnit)
 from ArmedForces af
 where year(af.DateOfEnlistment) between 2020 and 2022
 
